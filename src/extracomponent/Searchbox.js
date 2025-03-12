2db116bfe3fbd898018ccb939efdb772f0c77fc8
nileshmishra1234 <Nileshmishra9987@gmail.com>
2025-03-12 12:49:05 +0530

    first commit

--- src/extracomponent/Searchbox.js
+++ src/extracomponent/Searchbox.js
@@ -1,13 +1,28 @@
 import React, { useEffect } from 'react'
 import {useState} from 'react'
 import './Searchbox.css'
-import PlaceOutlinedIcon from '@mui/icons-material/PlaceOutlined';
-import AirIcon from '@mui/icons-material/Air';
-import WaterDropOutlinedIcon from '@mui/icons-material/WaterDropOutlined';
+import PlaceOutlinedIcon from '@mui/icons-material/PlaceOutlined'; //material ui icons
+import AirIcon from '@mui/icons-material/Air'; //material ui icons
+import WaterDropOutlinedIcon from '@mui/icons-material/WaterDropOutlined';  // //material ui icons
+
+// imgage for weather condition
+
+import clouds from "../clouds.png"
+import mist from "../cloud.png"
+import clear from "../clear.png"
+import rain from "../rain.png"
+import snow from "../snow.png"
+import overcast  from "../overcast.png"
+import brokencloud  from "../broken.png"
+import haze  from "../humidity.png"
+import 	thunderstorm  from "../thunderstorm.png"
+import 	scatteredclouds  from "../scattered.png"
+
+
 
 function Searchbox(props) {
   let [city , setCity]= useState('');
-  let [weather , setWeather]= useState({});
+  let [weatherdata , setWeatherdata]= useState({});
 
   let handlechanege = (evt)=>{
     console.log("hi")
@@ -15,33 +30,43 @@ function Searchbox(props) {
   };
 
   let handlesubmit =(evt)=>{
-    evt.preventDefault();
-    console.log(city);
-    getweatherinfo(city);
-    setCity("");
+    try{
+      evt.preventDefault();
+      console.log(city);
+      getweatherinfo(city);
+      setCity("");
+    }catch{
+      SetError(true)
+    }
+   
   }
 
   const API_URL = "https://api.openweathermap.org/data/2.5/weather";
   const API_KEY ="125be580cf56f636075936141071bf65";
 
   let getweatherinfo = async(city)=>{
-    let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
-    let jsonResponse = await response.json();
-    console.log(jsonResponse);
-
-    
-    let result ={
-      city:city,
-      temp:jsonResponse.main.temp,
-      tempMin:jsonResponse.main.temp_min,
-      tempMax:jsonResponse.main.temp_max,
-      humidity:jsonResponse.main.humidity,
-      wea:jsonResponse.weather[0].description,
-      icon:jsonResponse.weather[0].icon,
-      speed:jsonResponse.wind.speed
-    };
-    console.log(result);
-    setWeather(result);
+    try{
+      let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+      let jsonResponse = await response.json();
+      console.log(jsonResponse);
+  
+      
+      let result ={
+        city:city,
+        temp:jsonResponse.main.temp,
+        tempMin:jsonResponse.main.temp_min,
+        tempMax:jsonResponse.main.temp_max,
+        humidity:jsonResponse.main.humidity,
+        weatherdes:jsonResponse.weather[0].description,
+        // mai:jsonResponse.weather[0].main,   // main se bhi hum log weather condition img change kar sakta hai
+        // icon:jsonResponse.weather[0].icon,  // icon for feacting weather econdition img from website
+        speed:jsonResponse.wind.speed
+      };
+      console.log(result);
+      setWeatherdata(result);
+    }catch(err){
+      throw err;
+  }
   }
 
   useEffect(()=>{
@@ -62,32 +87,33 @@ function Searchbox(props) {
           <div className={`card-content`}>
             <div  className = {`card-title`}>
               <span><PlaceOutlinedIcon/></span>
-              <span><h3>  {weather.city} Weather </h3></span>
+              <span><h3>  {weatherdata.city} Weather </h3></span>
             </div>
             <div className = {`card-image` }>
-              <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="" />
+              <img src={weatherdata.weatherdes === "smoke"?clear:weatherdata.weatherdes === "scattered clouds"?scatteredclouds:weatherdata.weatherdes === "clear sky"?clear:weatherdata.weatherdes === "few clouds"?clouds:weatherdata.weatherdes === "shower rain"?rain:weatherdata.weatherdes === "overcast clouds"?overcast:weatherdata.weatherdes === "mist"?mist:weatherdata.weatherdes === "snow"?snow:weatherdata.weatherdes === "broken clouds"?brokencloud :weatherdata.weatherdes === "haze"?haze:weatherdata.weatherdes === "rain"?clouds :weatherdata.weatherdes === "thunderstorm"?thunderstorm:clear } alt="loDING" />
+              {/* <img src={`https://openweathermap.org/img/wn/${weatherdata.icon}@2x.png`} alt="" /> // feacting weather econdition img from website  */} 
             </div>
           
             <div className = {`temp_w`}>
-              <div className={`temperature`} >{weather.temp} ℃</div>
-              <div >{weather.wea}</div>
+              <div className={`temperature`} >{weatherdata.temp} ℃</div>
+              <div >{weatherdata.weatherdes}</div>
             </div>
           </div>
 
           <div className='temp_auto'>
           <div className='space-temp'>
             <div className = {`temp_M`}>
-              <div>Max Temp : {weather.tempMax}℃</div>
-              <div >Min Temp  : {weather.tempMin}℃</div>
+              <div>Max Temp : {weatherdata.tempMax}℃</div>
+              <div >Min Temp  : {weatherdata.tempMin}℃</div>
             </div>
             <div className = {`humidity_speep`}>
               <div className="huminity">
               <span><WaterDropOutlinedIcon/></span>
-              <span> Huminity : {weather.humidity}</span>
+              <span> Huminity : {weatherdata.humidity}</span>
               </div>             
               <div className='speed'>
               <span><AirIcon/></span>
-              <span > WindSpeed : {weather.speed} m/s</span>
+              <span > WindSpeed : {weatherdata.speed} m/s</span>
               </div>
             </div>
           </div>
